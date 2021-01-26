13563315b99eb9dbd32ec4a903c6ebdc4f8e579d
Renee <shanju.chou@gmail.com>
2021-01-26 11:31:54 +0800

    updated blog/ views

--- django_chirper/blog/views.py
+++ django_chirper/blog/views.py
@@ -1,5 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Post
+
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return HttpResponse('<h1>About Page</h1>')
