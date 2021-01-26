2ea3f7ac314e1b76a0bd913cf0f58c802bcf3329
Renee <shanju.chou@gmail.com>
2021-01-26 11:31:26 +0800

    created Post model

--- django_chirper/blog/models.py
+++ django_chirper/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.CharField(max_length = 280)
+    created_at = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    # if the author is deleted, so will all its posts; one-way only
+
+    def __repr__(self):
+        return f'{self.content} by {self.author};'
