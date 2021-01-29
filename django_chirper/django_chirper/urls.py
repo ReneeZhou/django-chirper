20a1be5c571b26a4799f26a78ecae780b05dc738
Renee <shanju.chou@gmail.com>
2021-01-29 17:53:45 +0800

    added login & logout paths

--- django_chirper/django_chirper/urls.py
+++ django_chirper/django_chirper/urls.py
@@ -14,12 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from user.forms import UserLoginForm
 from user import views as user_views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', user_views.signup, name = 'signup'),
+    # class based views
+    path('login/', \
+        auth_views.LoginView.as_view(template_name = 'login.html', authentication_form = UserLoginForm), \
+            name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
     path('', include('blog.urls')),
 ]
