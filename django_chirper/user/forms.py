b60d0ef93096e7661a62283891bfb246e0c02ea6
Renee <shanju.chou@gmail.com>
2021-01-29 17:52:34 +0800

    added custon UserLoginForm

--- django_chirper/user/forms.py
+++ django_chirper/user/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.forms.fields import CharField
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Div, HTML
@@ -24,21 +24,42 @@ class UserRegisterForm(UserCreationForm):
 
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Div(
-                CrispyField(
-                    Div(
-                        'username', 
-                        'email', 
-                        HTML('''
-                            <p class="text-blue-600 text-sm font-medium no-underline hover:underline cursor-pointer pb-4">User phone instead</p>
-                            '''),
-                        'password1',
-                        'password2',
-                        css_class = 'space-y-5'
-                    ),
+            CrispyField(
+                Div(
+                    'username', 
+                    'email', 
+                    HTML('''
+                        <p class="text-blue-600 text-sm font-medium no-underline hover:underline cursor-pointer pb-4">User phone instead</p>
+                        '''),
+                    'password1',
+                    'password2',
+                    css_class = 'space-y-5'
                 ),
-                Submit('submit', 'Sign Up', css_class = 'bg-blue-500 rounded-full h-12 w-128 font-bold outline-none \
-                    hover:bg-blue-600 cursor-pointer mt-6 self-center text-white'),
-            )
+            ),
+            Submit('submit', 'Sign Up', css_class = 'bg-blue-500 rounded-full h-12 w-128 font-bold outline-none \
+                hover:bg-blue-600 cursor-pointer mt-6 self-center text-white'),
         )
+
+
+class UserLoginForm(AuthenticationForm):
     
+    class Meta:
+        model = User
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.get('username').label = 'Phone, email, or username'
+        
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            CrispyField(
+                Div(
+                    'username',
+                    'password',
+                    css_class = 'space-y-5 w-128'
+                ),
+            ),
+            Submit('submit', 'Log in', \
+                css_class = 'rounded-full bg-blue-500 hover:bg-blue-600 active:bg-blue-700 w-128 h-12 \
+                font-bold outline-none cursor-pointer text-white mt-5'),
+        )
