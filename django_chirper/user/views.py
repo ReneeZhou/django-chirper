14cbcb1da344bb1bc35c9c495b084ebed33d95b6
Renee <shanju.chou@gmail.com>
2021-02-01 23:35:47 +0800

    removed '/' view & url in user app used built in auth from django

--- django_chirper/user/views.py
+++ django_chirper/user/views.py
@@ -4,22 +4,22 @@ from django.contrib.auth import authenticate, login
 from .forms import UserRegisterForm, HUserLoginForm
 
 
-def home_notauth(request):
-    if request.user.is_authenticated:
-        return redirect('home')
+# def home_notauth(request):
+#     if request.user.is_authenticated:
+#         return redirect('home')
     
-    form = HUserLoginForm()
-    if request.method == 'GET':
-        return render(request, 'home_notauth.html', {'form': form})
-    elif request.method == 'POST':
-        if form.is_valid():
-            return redirect('home')
-        else: 
-            messages.warning(request, 
-                            'There was unusual login activity on your account. \
-                                To help keep your account safe, \
-                                    please enter your phone number or email address to verify it’s you.')
-            return redirect('login')
+#     form = HUserLoginForm()
+#     if request.method == 'GET':
+#         return render(request, 'home_notauth.html', {'form': form})
+#     elif request.method == 'POST':
+#         if form.is_valid():
+#             return redirect('home')
+#         else: 
+#             messages.warning(request, 
+#                             'There was unusual login activity on your account. \
+#                                 To help keep your account safe, \
+#                                     please enter your phone number or email address to verify it’s you.')
+#             return redirect('login')
 
 
 def signup(request):
