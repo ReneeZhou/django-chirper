c9436c229768ee73a675201e33bb3894d8f78f17
Renee <shanju.chou@gmail.com>
2021-02-01 23:36:39 +0800

    modified project urls.py accordingly

--- django_chirper/django_chirper/urls.py
+++ django_chirper/django_chirper/urls.py
@@ -16,18 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from user.forms import UserLoginForm
-from user import views as user_views
+from user.forms import VUserLoginForm, HUserLoginForm
+# from user import views as user_views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', user_views.signup, name = 'signup'),
+    # path('signup/', user_views.signup, name = 'signup'),
     # class based views
-    path('login/', \
-        auth_views.LoginView.as_view(template_name = 'login.html', authentication_form = UserLoginForm), \
-            name = 'login'),
+    path('login/',
+        auth_views.LoginView.as_view(
+            template_name = 'login.html', 
+            authentication_form = VUserLoginForm
+            ), 
+        name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
-    path('', include('blog.urls')),
+    path('', auth_views.LoginView.as_view(
+        template_name = 'home_notauth.html', 
+        authentication_form = HUserLoginForm),
+        name = 'home_notauth'),
+    path('', include('main.urls')),
+    path('', include('user.urls')),
+    path('', include('user_profile.urls'))
 ]
