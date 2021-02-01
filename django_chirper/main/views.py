1c86e8e931259c484aff82bab3a2347aed2cc503
Renee <shanju.chou@gmail.com>
2021-02-01 23:26:25 +0800

    moved 'home/' view & url from blog app to main app

--- django_chirper/main/views.py
+++ django_chirper/main/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blog.models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+
+    if request.user.is_authenticated:
+        return render(request, 'home.html', context)
+    else: 
+        return render(request, 'home_notauth.html')
