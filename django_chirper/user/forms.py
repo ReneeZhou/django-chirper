f52991f5d33d12976684b8e7081c1fcda2d9e2ad
Renee <shanju.chou@gmail.com>
2021-02-02 16:16:31 +0800

    modified UserRegisterForm, added input style

--- django_chirper/user/forms.py
+++ django_chirper/user/forms.py
@@ -1,44 +1,59 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.forms.fields import CharField
+from django import forms
+from django.forms.fields import CharField, EmailField
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Div, HTML
 from crispy_forms.layout import Field as CrispyField
 
 
 class UserRegisterForm(UserCreationForm):
-    username = CharField(max_length = 50, label = 'Name')
+    input_field_class = 'bg-gray-700 bg-opacity-0 ml-2 mt-2 outline-none text-white'
+
+    username = CharField(
+        max_length = 50, 
+        label = 'Name',
+        widget = forms.TextInput(
+            attrs = {
+                'class': input_field_class
+            }
+        ))
+
+    email = EmailField(
+        label = 'Email', 
+        widget = forms.EmailInput(
+            attrs = {
+                'class': input_field_class
+            }
+        )
+    )
+
+    password1 = CharField(
+        max_length = 300,
+        label = 'Password',
+        widget = forms.PasswordInput(
+            attrs = {
+                'class': input_field_class
+            }
+        )
+    )
+
+    password2 = CharField(
+        max_length = 300, 
+        label = 'Confirm Password',
+        widget = forms.PasswordInput(
+            attrs = {
+                'class': input_field_class
+            }
+        )
+    )
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        labels = {
-            'email': 'Email',
-        }
-        
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields.get('password2').label = 'Confirm Password'
-        self.fields.get('password1').help_text = None
-        self.fields.get('password2').help_text = None
-
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            CrispyField(
-                Div(
-                    'username', 
-                    'email', 
-                    HTML('''
-                        <p class="text-blue-600 text-sm font-medium no-underline hover:underline cursor-pointer pb-4">User phone instead</p>
-                        '''),
-                    'password1',
-                    'password2',
-                    css_class = 'space-y-5'
-                ),
-            ),
-            Submit('submit', 'Sign Up', css_class = 'bg-blue-500 rounded-full h-12 w-128 font-bold outline-none \
-                hover:bg-blue-600 cursor-pointer mt-6 self-center text-white'),
-        )
 
 
 class VUserLoginForm(AuthenticationForm):
