310acf211d537290559e1de311fa23a6ec837722
Renee <shanju.chou@gmail.com>
2021-02-04 13:11:57 +0800

    setup media root for during development

--- django_chirper/django_chirper/urls.py
+++ django_chirper/django_chirper/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from user.forms import LoginForm
 
 
@@ -41,3 +43,7 @@ urlpatterns = [
     path('', include('user.urls')),
     path('', include('user_profile.urls'))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
