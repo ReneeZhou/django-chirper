32195ecaa463d74d68e0a23e1ea6f4ea1d91b0a5
Renee <shanju.chou@gmail.com>
2021-02-04 13:04:57 +0800

    modified Profile model, allowed blank cols

--- django_chirper/user_profile/models.py
+++ django_chirper/user_profile/models.py
@@ -7,9 +7,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     profile_image = models.ImageField(default = 'default_profile.png', upload_to = 'profile_pics')
     background_image = models.ImageField(default = 'default_background.png', upload_to = 'background_pics')
-    bio = models.TextField(max_length = 160)
-    location = models.TextField(max_length = 30)
-    website = models.URLField(max_length = 100)
+    bio = models.TextField(max_length = 160, blank = True)
+    location = models.TextField(max_length = 30, blank = True)
+    website = models.URLField(max_length = 100, blank = True)
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
