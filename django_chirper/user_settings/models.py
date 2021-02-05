4bfd2fc4e44f25ad675ef96a6ffa4c91741dc3d3
Renee <shanju.chou@gmail.com>
2021-02-05 15:31:22 +0800

    overrode models.Model.save() to shrink img size

--- django_chirper/user_settings/models.py
+++ django_chirper/user_settings/models.py
@@ -1,6 +1,6 @@
+from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
-
 
 
 class Profile(models.Model):
@@ -10,6 +10,21 @@ class Profile(models.Model):
     bio = models.TextField(max_length = 160, blank = True)
     location = models.TextField(max_length = 30, blank = True)
     website = models.URLField(max_length = 100, blank = True)
+
+    # override save() in the Model class
+    def save(self):
+        super().save()                                                      # first run the parent's save method
+        output_size = {'profile': (400, 400), 'background': (600, 200)}
+        p_img = Image.open(self.profile_image.path)
+        b_img = Image.open(self.background_image.path)
+
+        if p_img.width > 400 or p_img.height > 400:
+            p_img.thumbnail(output_size.get('profile'))
+            p_img.save(self.profile_image.path)
+
+        if b_img.width > 600 or p_img.height > 200:
+            b_img.thumbnail(output_size.get('background'))
+            b_img.save(self.background_image.path)
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
