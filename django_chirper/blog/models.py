e25d1d613af4b494b8aa4b58611ad1aae9a4c0d4
Renee <shanju.chou@gmail.com>
2021-02-08 17:44:49 +0800

    customized Post model id & migrated

--- django_chirper/blog/models.py
+++ django_chirper/blog/models.py
@@ -1,9 +1,15 @@
+from secrets import randbits
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+def gen_key(bits = 60):
+    return randbits(bits)
+
+
 class Post(models.Model):
+    id = models.BigAutoField(primary_key = True, default = gen_key)
     content = models.CharField(max_length = 280)
     created_at = models.DateTimeField(default = timezone.now)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
