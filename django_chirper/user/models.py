f57e7ffcb154b3f1264de70ba311dbe8a5ba1a00
Renee <shanju.chou@gmail.com>
2021-02-08 17:39:45 +0800

    moved Profile model from user_settings -> user app added more fields

--- django_chirper/user/models.py
+++ django_chirper/user/models.py
@@ -1,3 +1,49 @@
+from secrets import randbits, token_urlsafe
+from PIL import Image
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def gen_key(bits = 60):
+    return randbits(bits)
+    
+def gen_hex(bytes = 5):
+    return token_urlsafe(bytes)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    id = models.AutoField(primary_key = True, default = gen_key)
+    created_at_ip = models.GenericIPAddressField(null = True, blank = True)
+    handle = models.TextField(default = gen_hex, unique = True)
+    phone = models.CharField(max_length = 20, null = True, blank = True, unique = True)
+    birthdate = models.DateField(null = True, blank = True)
+
+    profile_image = models.ImageField(default = 'default_profile.png', upload_to = 'profile_pics')
+    background_image = models.ImageField(default = 'default_background.png', upload_to = 'background_pics')
+    bio = models.TextField(max_length = 160, blank = True)
+    location = models.TextField(max_length = 30, blank = True)
+    website = models.URLField(max_length = 100, blank = True)
+
+    # override save() in the Model class
+    def save(self, *args, **kwargs):
+        super().save()                                                      # first run the parent's save method
+        output_size = {'profile': (400, 400), 'background': (600, 200)}
+        p_img = Image.open(self.profile_image.path)
+        b_img = Image.open(self.background_image.path)
+
+        if p_img.width > 400 or p_img.height > 400:
+            p_img.thumbnail(output_size.get('profile'))
+            p_img.save(self.profile_image.path)
+
+        if b_img.width > 600 or p_img.height > 200:
+            b_img.thumbnail(output_size.get('background'))
+            b_img.save(self.background_image.path)
+
+    def __str__(self):
+        return f'{self.user.username}\'s Profile'
+
+    def __repr__(self):
+        return f'{self.user.username}\'s Profile' 
