e996e98f89684b7e8ed98e60bedfd3fd12d01757
Renee <shanju.chou@gmail.com>
2021-02-10 21:20:09 +0800

    added profile views, urls & templates profile/ - _withReplies/, _media/ & _likes/

--- django_chirper/user_profile/urls.py
+++ django_chirper/user_profile/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('<str:handle>/', views.profile, name = 'profile')
+    path('<str:handle>/', views.profile, name = 'profile'),
+    path('<str:handle>/with_replies/', views.profile_withReplies, name = 'profile_withReplies'),
+    path('<str:handle>/media/', views.profile_media, name = 'profile_media'), 
+    path('<str:handle>/likes', views.profile_likes, name = 'profile_likes')
 ]
--- django_chirper/user_profile/views.py
+++ django_chirper/user_profile/views.py
@@ -8,8 +8,44 @@ def profile(request, handle):
         user_profile = Profile.objects.get(handle = handle)
         if user_profile:
             context = {
-                'user': user_profile.user
+                'user_profile': user_profile
             }
             return render(request, 'profile.html', context)
+    except Profile.DoesNotExist:
+        return page_not_found(request, None, 'errors/404.html')
+
+
+def profile_withReplies(request, handle):
+    try: 
+        user_profile = Profile.objects.get(handle = handle)
+        if user_profile:
+            context = {
+                'user_profile': user_profile
+            }
+            return render(request, 'profile_withReplies.html', context)
+    except Profile.DoesNotExist:
+        return page_not_found(request, None, 'errors/404.html')
+
+
+def profile_media(request, handle):
+    try: 
+        user_profile = Profile.objects.get(handle = handle)
+        if user_profile:
+            context = {
+                'user_profile': user_profile
+            }
+            return render(request, 'profile_likes.html', context)
+    except Profile.DoesNotExist:
+        return page_not_found(request, None, 'errors/404.html')
+
+
+def profile_likes(request, handle):
+    try: 
+        user_profile = Profile.objects.get(handle = handle)
+        if user_profile:
+            context = {
+                'user_profile': user_profile
+            }
+            return render(request, 'profile_likes.html', context)
     except Profile.DoesNotExist:
         return page_not_found(request, None, 'errors/404.html')
