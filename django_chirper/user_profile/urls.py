431f04fa5827fa8e5c5ad17debdea9efe6426133
Renee <shanju.chou@gmail.com>
2021-02-12 01:55:05 +0800

    modified profile view to class based

--- django_chirper/user_profile/urls.py
+++ django_chirper/user_profile/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import ProfilePostView
 
 
 urlpatterns = [
-    path('<str:handle>/', views.profile, name = 'profile'),
+    path('<str:handle>/', ProfilePostView.as_view(), name = 'profile'),
     path('<str:handle>/with_replies/', views.profile_withReplies, name = 'profile_withReplies'),
     path('<str:handle>/media/', views.profile_media, name = 'profile_media'), 
     path('<str:handle>/likes', views.profile_likes, name = 'profile_likes')
--- django_chirper/user_profile/views.py
+++ django_chirper/user_profile/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.views.defaults import page_not_found
 from user.models import Profile
+from blog.models import Post
 
 
-def profile(request, handle):
-    try: 
-        user_profile = Profile.objects.get(handle = handle)
-        if user_profile:
-            context = {
-                'user_profile': user_profile
-            }
-            return render(request, 'profile.html', context)
-    except Profile.DoesNotExist:
-        return page_not_found(request, None, 'errors/404.html')
+class ProfilePostView(ListView):
+    template_name = 'profile.html'
+
+    def get_queryset(self):
+        self.user_profile = get_object_or_404(Profile, handle = self.kwargs['handle'])    # from the url
+        return Post.objects.filter(id = self.user_profile.user.id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_profile'] = self.user_profile
+        return context
 
 
 def profile_withReplies(request, handle):
