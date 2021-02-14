e8d8c86448433ace6f7198a903fc657376964853
Renee <shanju.chou@gmail.com>
2021-02-14 23:32:04 +0800

    added status template, view & url

--- django_chirper/blog/urls.py
+++ django_chirper/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import StatusDetailView
 
 
 urlpatterns = [
+    path('<str:handle>/status/<int:pk>/', StatusDetailView.as_view(), name = 'status'),
     path('compose/chirp/', views.compose_chirp, name = 'compose_chirp')
 ]
--- django_chirper/blog/views.py
+++ django_chirper/blog/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView
+from user.models import Profile
 from .forms import PostForm
+from .models import Post
+
+
+class StatusDetailView(DetailView):
+    template_name = 'status.html'
+
+    def get_queryset(self):
+        self.user_profile = get_object_or_404(Profile, handle = self.kwargs['handle'])
+        self.post = get_object_or_404(Post, id = self.kwargs['pk'])
+        return Post.objects.filter(id = self.post.id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = self.post
+        context['user_profile'] = self.user_profile
+        return context
 
 
 @login_required
