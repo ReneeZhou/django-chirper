063464464df8aaaaf9028494142a891421c45eb0
Renee <shanju.chou@gmail.com>
2021-02-25 15:25:58 +0800

    updated URLconfig accordingly

--- django_chirper/user/urls.py
+++ django_chirper/user/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
 from django.urls import path
 from . import views
-
+from .views import UserCreateView
 
 urlpatterns = [
     path('', views.home_notauth, name = 'home_notauth'),
-    path('signup/', views.signup, name = 'signup'),
+    path('signup/', UserCreateView.as_view(), name = 'signup')
 ]
