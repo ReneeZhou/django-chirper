829b42ff77b61fb6135b475d4f653a21c796c237
Renee <shanju.chou@gmail.com>
2021-02-26 21:28:07 +0800

    registered Follower model, added inline to Profile

--- django_chirper/user/admin.py
+++ django_chirper/user/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Follower
 
+class FollowerInline(admin.TabularInline):
+    model = Follower
+    fk_name = 'following'
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [FollowerInline]
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Follower)
