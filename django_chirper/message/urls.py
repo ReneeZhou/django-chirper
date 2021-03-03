b003afc5e8de3010b98eaee70c22e50c35a41053
Renee <shanju.chou@gmail.com>
2021-03-04 02:12:33 +0800

    added message app views, urls & templates

--- django_chirper/message/urls.py
+++ django_chirper/message/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    path('messages/', views.messages, name = 'messages')
+    path('messages/', views.messages, name = 'messages'),
+    path(
+        'messages/<int:counterpart_id>-<int:currentuser_id>/', 
+        views.messages_counterpart, 
+        name = 'messages_counterpart'
+    ),
+    path(
+        'messages/<int:counterpart_id>-<int:currentuser_id>/info/', 
+        views.messages_counterpart_info, 
+        name = 'messages_counterpart_info'
+    )
 ]
--- django_chirper/message/views.py
+++ django_chirper/message/views.py
@@ -1,10 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from user.models import Profile
 
 
 @login_required
 def messages(request):
     request.user.profile.last_checked_message_at = timezone.now()
     request.user.profile.save()
-    return render(request, 'messages.html')
+
+    following_profiles = request.user.profile.following.all()
+    context = {
+        'following_profiles': following_profiles
+    }
+    return render(request, 'messages.html', context = context)
+
+
+@login_required
+def messages_counterpart(request, counterpart_id, currentuser_id):
+    counterpart = Profile.objects.get(id = counterpart_id)
+
+    if counterpart is None: 
+        return redirect('messages')
+    else: 
+        following_profiles = request.user.profile.following.all()
+        message_history = list(request.user.profile.sent.all()) + list(request.user.profile.received.all())
+        context = {
+            'counterpart': counterpart,
+            'following_profiles': following_profiles,
+            'message_history': message_history
+        }
+
+    return render(request, 'messages_counterpart.html', context)
+
+
+@login_required
+def messages_counterpart_info(request, counterpart_id, currentuser_id):
+    counterpart = Profile.objects.get(id = counterpart_id)
+
+    if counterpart is None: 
+        return redirect('messages')
+    else: 
+        following_profiles = request.user.profile.following.all()
+        context = {
+            'counterpart': counterpart,
+            'following_profiles': following_profiles
+        }
+
+    return render(request, 'messages_counterpart_info.html', context)
