94d04347f87c6fb15a35c398ab1b15702d9b6073
Renee <shanju.chou@gmail.com>
2021-03-09 23:27:37 +0800

    created BaseMessageView

--- django_chirper/message/views.py
+++ django_chirper/message/views.py
@@ -1,5 +1,7 @@
+from django.db.models.expressions import OuterRef
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.db.models import Q, When, Case
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
@@ -8,7 +10,14 @@ from message.models import Message
 from message.forms import MessageForm
 
 
-class MessageView(LoginRequiredMixin, ListView):
+class BaseMessageView(LoginRequiredMixin, ListView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['following_profiles'] = self.request.user.profile.following.all()
+        return context
+
+
+class MessageView(BaseMessageView):
     template_name = 'messages.html'
 
     # update timestamp everytime user check msg page, need to cascade down to messages/.../
@@ -16,11 +25,6 @@ class MessageView(LoginRequiredMixin, ListView):
         self.request.user.profile.last_checked_message_at = timezone.now()
         self.request.user.save()
         return 
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['following_profiles'] = self.request.user.profile.following.all()
-        return context
 
 
 @login_required
@@ -53,7 +57,7 @@ def messages_counterpart(request, counterpart_id, currentuser_id):
     return render(request, 'messages_counterpart.html', context)
 
 
-class MessageCounterpartInfoView(LoginRequiredMixin, ListView):
+class MessageCounterpartInfoView(BaseMessageView):
     template_name = 'messages_counterpart_info.html'
     
     def get_queryset(self):
@@ -62,19 +66,13 @@ class MessageCounterpartInfoView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['following_profiles'] = self.request.user.profile.following.all()
         context['counterpart'] = Profile.objects.get(id = self.counterpart_id)
         return context
         
 
-class MessageComposeView(LoginRequiredMixin, ListView):
+class MessageComposeView(BaseMessageView):
     template_name = 'messages_compose.html'
 
     def get_queryset(self):
         self.following_profiles = self.request.user.profile.following.all()
         return self.following_profiles
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['following_profiles'] = self.following_profiles
-        return context
