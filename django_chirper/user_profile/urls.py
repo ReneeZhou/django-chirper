d76bbd392d551ac7e21dafd8149251dac31fa8cc
Renee <shanju.chou@gmail.com>
2021-03-19 00:16:03 +0800

    modified URLconfig

--- django_chirper/user_profile/urls.py
+++ django_chirper/user_profile/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('media/', ProfileMediaView.as_view(), name = 'profile_media'), 
     path('likes/', ProfileLikesView.as_view(), name = 'profile_likes'),
     path('followers/', ProfileFollowersView.as_view(), name = 'profile_followers'),
-    path('followers/', ProfileFollowingView.as_view(), name = 'profile_following'),
-    path('followers/', ProfileFollowersYouKnowView.as_view(), name = 'profile_followersYouKnow'),
-    path('followers/', ProfileSuggestedView.as_view(), name = 'profile_suggested'),
+    path('following/', ProfileFollowingView.as_view(), name = 'profile_following'),
+    path('followers_you_know/', ProfileFollowersYouKnowView.as_view(), name = 'profile_followersYouKnow'),
+    path('suggested/', ProfileSuggestedView.as_view(), name = 'profile_suggested'),
     path('follow/', views.follow, name = 'follow'),
     path('unfollow/', views.unfollow, name = 'unfollow')
 ]
