f660ed3eb637f88acc8446e7acfa598e16207b1f
Renee <shanju.chou@gmail.com>
2021-03-23 16:42:52 +0800

    added user_settings app views & urls

--- django_chirper/user_settings/urls.py
+++ django_chirper/user_settings/urls.py
@@ -3,5 +3,32 @@ from . import views
 
 
 urlpatterns = [
-    path('settings/profile/', views.settings_profile, name = 'settings_profile')
+    path('', views.settings, name = 'settings'),
+    path('account/', views.settings_account, name = 'settings_account'),
+    path('account/personalization/', views.settings_account_personalization, name = 'settings_account_personalization'),
+    path('password/', views.settings_password, name = 'settings_password'),
+    path('screenName/', views.settings_screenName, name = 'settings_screenName'),
+    path('security_and_account_access/', views.settings_securityAndAccountAccess, name = 'settings_securityAndAccountAccess'),
+    path('privacy_and_safety', views.settings_privacyAndSafety, name = 'settings_provacyAndSafety'),
+    path('notifications/', views.settings_notifications, name = 'settings_notifications'),
+    path('accessibility_display_and_languages', views.settings_accessibilityDisplayAndLanguages, name = 'settings_accessibilityDisplayAndLanguages'),
+    path('about/', views.settings_about, name = 'settings_about'),
+    path('your_chirper_data/', views.settings_yourChirperData, name = 'settings_yourChirperData'),
+    path('your_chirper_data/account/', views.settings_yourChirperData_account, name = 'settings_yourChirperData_account'),
+    path('your_chirper_data/gender/', views.settings_yourChirperData_gender, name = 'settings_yourChirperData_gender'),
+    path('your_chirper_data/age/', views.settings_yourChirperData_age, name = 'settings_yourChirperData_age'),
+    path('phone/', views.settings_phone, name = 'settings_phone'),
+    path('add_phone/auth/', views.settings_addPhone_auth, name = 'settings_addPhone_auth'),
+    path('add_phone/', views.settings_addPhone, name = 'settings_addPhone'),
+    path('delete_phone/', views.settings_deletePhone, name = 'settings_deletePhone'),
+    path('email/', views.settings_email, name = 'settings_email'),
+    path('add_email/auth/', views.settings_addEmail_auth, name = 'settings_addEmail_auth'),
+    path('add_email/', views.settings_addEmail, name = 'settings_addEmail'),
+    path('audience_and_tagging/', views.settings_audienceAndTagging, name = 'settings_audienceAndTagging'),
+    path('country/', views.settings_country, name = 'settings_country'),
+    path('languages/', views.settings_languages, name = 'settings_languages'),
+    path('trends/', views.settings_trends, name = 'settings_trends'),
+    path('content_preferences/', views.settings_contentPreferences, name = 'settings_contentAndPreferences'),
+    path('apps_and_sessions/', views.settings_appsAndSessions, name = 'settings_appsAndSessions'),
+    path('profile/', views.settings_profile, name = 'settings_profile')
 ]
--- django_chirper/user_settings/views.py
+++ django_chirper/user_settings/views.py
@@ -1,7 +1,143 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UpdateUsernameForm, UpdateProfileForm
 
+
+def settings(request):
+    if request.user.is_authenticated:
+        return redirect('settings_account')
+    else:
+        return redirect('settings_account_personalization')
+
+
+@login_required
+def settings_account(request):
+    return render(request, 'settings_account.html')
+
+
+def settings_account_personalization(request):
+    return render(request, 'settings_account_personalization.html')
+
+
+@login_required
+def settings_password(request):
+    return render(request, 'settings_password.html')
+
+
+@login_required
+def settings_screenName(request):
+    return render(request, 'settings_screenName.html')
+
+
+@login_required
+def settings_securityAndAccountAccess(request):
+    return render(request, 'settings_securityAndAccountAccess.html')
+
+
+@login_required
+def settings_privacyAndSafety(request):
+    return render(request, 'settings_privacyAndSafety.html')
+
+
+@login_required
+def settings_notifications(request):
+    return render(request, 'settings_notifications.html')
+
+
+@login_required
+def settings_accessibilityDisplayAndLanguages(request):
+    return render(request, 'settings_accessibilityDisplayAndLanguages.html')
+
+
+def settings_about(request):
+    return render(request, 'settings_about.html')
+
+
+@login_required
+def settings_yourChirperData(request):
+    return render(request, 'settings_yourChirperDate.html')
+
+
+@login_required
+def settings_yourChirperData_account(request):
+    return render(request, 'settings_yourChirperData_account.html')
+
+
+@login_required
+def settings_phone(request):
+    return render(request, 'settings_phone.html')
+
+
+@login_required
+def settings_addPhone_auth(request):
+    return render(request, 'settings_addPhone_auth.html')
+
+
+@login_required
+def settings_addPhone(request):
+    return render(request, 'settings_addPhone.html')
+
+
+@login_required
+def settings_deletePhone(request):
+    return render(request, 'settings_deletePhone.html')
+
+
+
+@login_required
+def settings_email(request):
+    return render(request, 'settings_email.html')
+
+
+@login_required
+def settings_addEmail_auth(request):
+    return render(request, 'settings_addEmail_auth.html')
+
+
+@login_required
+def settings_addEmail(request):
+    return render(request, 'settings_addEmail.html')
+
+
+@login_required
+def settings_audienceAndTagging(request):
+    return render(request, 'settings_audienceAndTagging.html')
+
+
+@login_required
+def settings_country(request):
+    return render(request, 'settings_country.html')
+
+
+@login_required
+def settings_languages(request):
+    return render(request, 'settings_languages.html')
+
+
+@login_required
+def settings_yourChirperData_gender(request):
+    return render(request, 'settings_yourChirperData_gender.html')
+
+
+@login_required
+def settings_yourChirperData_age(request):
+    return render(request, 'settings_yourChirperData_age.html')
+
+
+@login_required
+def settings_trends(request):
+    return render(request, 'settings_trends.html')
+
+
+@login_required
+def settings_contentPreferences(request):
+    return render(request, 'settings_contentPreferences.html')
+
+
+@login_required
+def settings_appsAndSessions(request):
+    return render(request, 'settings_appsAndSessions.html')
 
 
 @login_required
