67e7593176c4bfc08ceb404e4f29ae31e4d15c93
Renee <shanju.chou@gmail.com>
2021-03-31 02:24:17 +0800

    added views to user_account app - BeginPasswordResetView & SendPasswordReserView

--- django_chirper/user_account/views.py
+++ django_chirper/user_account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.contrib.auth.views import (PasswordResetView, PasswordResetDoneView,
+    PasswordResetConfirmView, PasswordResetCompleteView)
+from .forms import BeginPasswordResetForm
 
-# Create your views here.
+
+class BeginPasswordResetView(FormView):
+    template_name = 'account_beginPasswordReset.html'
+    form_class = BeginPasswordResetForm
+    success_url = reverse_lazy('account_sendPasswordReset')
+
+
+class SendPasswordResetView(PasswordResetView):
+    template_name = 'account_sendPasswordReset.html'
