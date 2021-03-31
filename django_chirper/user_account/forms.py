cbcd1c1b59e718251a7d49e64449b7fdf2a6bdf5
Renee <shanju.chou@gmail.com>
2021-04-01 02:48:54 +0800

    added ResetPasswordForm

--- django_chirper/user_account/forms.py
+++ django_chirper/user_account/forms.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.forms import PasswordResetForm
 from django.core.exceptions import ValidationError
-from django.forms.fields import EmailField
-from django.forms.widgets import EmailInput
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
+from django.forms.fields import CharField, EmailField, BooleanField
+from django.forms.widgets import EmailInput, PasswordInput, CheckboxInput
 from django.contrib.auth.models import User
 
 
 class BeginPasswordResetForm(PasswordResetForm):
     email = EmailField(
-        label = 'Email' ,
+        label = 'Email',
         max_length = 254,
         widget = EmailInput(
             attrs = {
@@ -21,6 +22,47 @@ class BeginPasswordResetForm(PasswordResetForm):
         if not User.objects.filter(email = data).all():
             raise ValidationError("We couldn't find your account with that information.")
         return data
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class ResetPasswordForm(SetPasswordForm):
+    input_field_class = 'border border-gray-400 rounded-full focus:border-blue-500 focus:outline-none w-88 h-10 pl-3'
+
+    new_password1 = CharField(
+        label = 'New password',
+        widget = PasswordInput(
+            attrs = {
+                'autocomplete': 'new-password',
+                'class': input_field_class
+            }
+        ),
+        strip = False,
+        help_text = password_validation.password_validators_help_text_html(),
+    )
+
+    new_password2 = CharField(
+        label = 'New password confirmation',
+        strip = False,
+        widget = PasswordInput(
+            attrs = {
+                'autocomplete': 'new-password',
+                'class': input_field_class
+            }
+        ),
+    )
+
+    remember = BooleanField(
+        label = 'Remember me',
+        widget = CheckboxInput(
+            attrs = {
+                'checked': 'checked',
+                'class': 'form-checkbox'
+            }
+        )
+    )
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
