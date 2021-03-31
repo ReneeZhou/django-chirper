62e57038429766e1dd78ae7add75f95964cf4981
Renee <shanju.chou@gmail.com>
2021-04-01 02:51:06 +0800

    modified urlpatterns

--- django_chirper/user_account/urls.py
+++ django_chirper/user_account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import (PasswordResetView, PasswordResetDoneView,
     PasswordResetConfirmView, PasswordResetCompleteView)
-from .views import (BeginPasswordResetView, SendPasswordResetView)
+from .views import (BeginPasswordResetView, ConfirmPasswordResetView, ResetPasswordView)
 
 
 urlpatterns = [
     path('begin_password_reset/', BeginPasswordResetView.as_view(), name = 'account_beginPasswordReset'),
-    path('send_password_reset/', SendPasswordResetView.as_view(), name = 'account_sendPasswordReset'),
+    path('confirm_password_reset/', ConfirmPasswordResetView.as_view(), name = 'account_confirmPasswordReset'),
+    path('reset_password/<uidb64>/<token>/', ResetPasswordView.as_view(), name = 'password_reset_confirm'),
+    # name = 'password_reset_confirm' can't be customized
 ]
