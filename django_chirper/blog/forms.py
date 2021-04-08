98590c0013d432bb284cf5f64291e38db43d8ebc
Renee <shanju.chou@gmail.com>
2021-04-08 22:27:25 +0800

    created PostReplyForm

--- django_chirper/blog/forms.py
+++ django_chirper/blog/forms.py
@@ -17,3 +17,19 @@ class PostForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+# same as PostForm, only difference at placeholder
+class PostReplyForm(PostForm):
+    class Meta:
+        model = Post
+        fields = ['content']
+        widgets = {
+            'content': Textarea(attrs = {
+                'rows': 6, 
+                'placeholder': "Chirp your reply",
+                'class': 'resize-none bg-gray-900 text-xl focus:outline-none p-2'
+            })
+        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
